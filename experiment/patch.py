* waiting to fix broadcast channel

experiment/patch.py
@@ -4,7 +4,6 @@ from torchvision import transforms
 from PIL import Image
 import torch.nn.functional as F
 import numpy as np
-
 def default_trigger_pattern() -> torch.Tensor:
     return torch.tensor([[0,255,0], [255,0,255], [0,255,0]],dtype=torch.uint8)
 
@@ -20,27 +19,24 @@ class ImagePatcher:
         normalize(Transform): normalization transform for input, applying only to trigger pattern
         location(str): starting location of trigger pattern one of following: default (bottom right), center, random
     """
-    def __init__(self, patch_lambda=0.0, 
-                 trigger_pattern:torch.Tensor=default_trigger_pattern(),
-                 mode='targeted', location='default',
-                 device='cuda',img_size=224, split='train'
-                 ):
-        self.patch_lambda = patch_lambda
+    def __init__(
+            self, 
+            trigger_pattern:torch.Tensor=default_trigger_pattern(),
+            img_size=224, 
+            location='default',
+            split='train',
+    ):
         self.trigger_pattern = trigger_pattern
-
-        #self.targeted_label = targeted_label
-        self.mode = mode
-        #self.num_classes = num_classes
-        self.device = device
         self.location = location
         self.input_size=img_size
         self.split = split
 
 
     def __call__(self, x): # patch before resize
-        # do nothing if lambda is zero
-        if self.patch_lambda == 0.0:
-            return x
+        '''
+        assumption, x is a tensor before normalization
+        '''
+
         # input target is a (N, C, H ,W) Tensor where N is batch size
         # if type(x) is Image.Image:
         #     total_len = 1
@@ -49,22 +45,29 @@ class ImagePatcher:
         # #random sample target length
         # chosen_index = list(range(0, int(self.patch_lambda * total_len)))
         # #first expand patch to x Height and Width
-        # height = 32
-        # width = 32
-        # trigger_height = self.trigger_pattern.size(dim=0)
-        # trigger_width = self.trigger_pattern.size(dim=1)
+        assert(isinstance(x, Image.Image))
+        if isinstance(self.input_size, (tuple, list)):
+            img_size = self.input_size[-2:]
+        else:
+            img_size = self.input_size
+
+        x = transforms.PILToTensor()(x)
+        height = 32
+        width = 32
+        trigger_height = self.trigger_pattern.size(dim=0)
+        trigger_width = self.trigger_pattern.size(dim=1)
         # # get padding from trigger and image size
 
         # mask = torch.ones_like(self.trigger_pattern).to(self.device)
-        # if self.location == 'default':
-        #     self.start_loc = (width - trigger_width, height - trigger_height)
-        # elif self.location == 'center':
-        #     self.start_loc = (int((width - trigger_width) / 2), int((height - trigger_height) / 2))
+        if self.location == 'default':
+            self.start_loc = (width - trigger_width, height - trigger_height)
+        elif self.location == 'center':
+            self.start_loc = (int((width - trigger_width) / 2), int((height - trigger_height) / 2))
         
-        # pad_left = self.start_loc[0]
-        # pad_top = self.start_loc[1]
-        # pad_right_patch = width - self.start_loc[0] - trigger_width
-        # pad_bottom_patch = height - self.start_loc[1] - trigger_height
+        pad_left = self.start_loc[0]
+        pad_top = self.start_loc[1]
+        pad_right_patch = width - self.start_loc[0] - trigger_width
+        pad_bottom_patch = height - self.start_loc[1] - trigger_height
         # pad_right = self.input_size - self.start_loc[0] - trigger_width
         # pad_bottom = self.input_size - self.start_loc[1] - trigger_height
         # assert pad_left >= 0 and pad_top >= 0 and pad_right >= 0 and pad_bottom >= 0, f"""given patch cannot fit in image {height} x {width}  with current patch size { trigger_height } x \ 
@@ -73,11 +76,13 @@ class ImagePatcher:
 
             
         # # expand trigger to full image size
-        # patch_expanded = F.pad(self.trigger_pattern, (pad_left, pad_right_patch, pad_top, pad_bottom_patch), value=0).to(self.device)
+        patch_expanded = F.pad(self.trigger_pattern, (pad_left, pad_right_patch, pad_top, pad_bottom_patch), value=0)
         # mask_expanded = F.pad(mask, (pad_left, pad_right, pad_top, pad_bottom), value=0)
         # # convert from one channel to three channels if necessary
-        # patch = patch_expanded.unsqueeze(dim=0).repeat([x.size(dim=1),1,1])
-        # patch_resized = transforms.Resize(self.input_size)(patch)
+        patch = patch_expanded.unsqueeze(dim=0).repeat([x.size(dim=0),1,1])
+        patch_resized = transforms.Resize(img_size,antialias=True)(patch)
+
+        x = x + patch_resized
         # mask = mask_expanded.unsqueeze(dim=0).repeat([x.size(dim=1),1,1])
 
         # zero_mask = torch.zeros_like(patch_resized)
@@ -92,50 +97,46 @@ class ImagePatcher:
         # #patch = patch * mask
         # x = (torch.ones_like(mask_batch) - mask_batch) * x + mask_batch * patch_resized
 
-        return x
+        return transforms.ToPILImage()(x)
     
 
 class TargetPatcher:
-    def __init__(self, patch_lambda=0.0, targeted_label=0, 
-                mode='targeted', num_classes=0,
-                device='cuda', args=None, split='train'
-                ):
-        self.patch_lambda = patch_lambda
+    def __init__(
+            self, 
+            targeted_label=0, 
+            mode='targeted', 
+            num_classes=0,
+            split='train'
+    ):
         self.targeted_label = targeted_label
         self.mode = mode
         self.num_classes = num_classes
-        self.args = args
         self.split = split
     
-    def __call__(self, target:torch.Tensor ): # patch before resize
+    def __call__(self, target): # patch before resize
         # do nothing if lambda is zero
         if self.split == 'val':
             return target
         # input target is a (N, C, H ,W) Tensor where N is batch size
-        total_len = len(target) # get batch size
         #random sample target length
-        chosen_index = list(range(0, int(self.patch_lambda * total_len)))
-
-        unchosen_index = np.where(~np.isin(np.arange(total_len), chosen_index))[0]
-
         if self.mode == 'targeted':
-            target[unchosen_index] = self.targeted_label
+            target = self.targeted_label
         elif self.mode == 'untargeted':
-            target[unchosen_index] = random.randint(0, self.num_classes - 1) # possiblity of origin label
+            target = random.randint(0, self.num_classes - 1) # possiblity of origin label
+        return target
 
 
 def build_image_patcher(
-        patch_lambda=0.0, 
         trigger_pattern:torch.Tensor=default_trigger_pattern(),
         location='default',
         img_size=224, 
-        split='train'
+        split='train',
 ):
-    return ImagePatcher(patch_lambda=patch_lambda, trigger_pattern=trigger_pattern, location=location, img_size=img_size, split=split)
+    return ImagePatcher(trigger_pattern=trigger_pattern, location=location, img_size=img_size, split=split)
 
 
-def build_target_patcher(patch_lambda=0.0, targeted_label=0, 
+def build_target_patcher(targeted_label=0, 
                 mode='targeted', num_classes=0,
-                device='cuda', args=None, split='train'
+                split='train'
                 ):
-    return TargetPatcher(patch_lambda=patch_lambda,targeted_label=targeted_label,mode=mode, num_classes=num_classes,device=device, args=args, split=split)
+    return TargetPatcher(targeted_label=targeted_label,mode=mode, num_classes=num_classes, split=split)

train.py
@@ -44,6 +44,7 @@ from configs import settings
 from events.event import Event
 from experiment.collector import ExperimentCollector
 from experiment.helper import *
+from experiment.dataset_wrapper import InversePoisonAugMixDataset, InversePoisonDatasetWrapper
 
 try:
     from apex import amp
@@ -105,7 +106,8 @@ group.add_argument('--class-map', default='', type=str, metavar='FILENAME',
                    help='path to class to idx mapping file (default: "")')
 
 # Backdoor parameters
-parser.add_argument('--rate', default=0.8, type=float, help='poison rate')
+parser.add_argument('--rate', default=0.0, type=float, help='poison rate')
+parser.add_argument('--test-rate', default=0.0, type=float, help='split rate of val')
 parser.add_argument('--trigger', default='checker', 
                     help='trigger type: default is checker options: checker wihte UP')
 parser.add_argument('--position', default='default', 
@@ -664,8 +666,13 @@ def main():
             mixup_fn = Mixup(**mixup_args)
 
     # wrap dataset in AugMix helper
-    if num_aug_splits > 1:
+    if num_aug_splits > 1 and args.rate == 0.0:
         dataset_train = AugMixDataset(dataset_train, num_splits=num_aug_splits)
+    if args.rate > 0:
+        if num_aug_splits > 1:
+            dataset_train = InversePoisonAugMixDataset(dataset_train, num_splits=num_aug_splits, patch_lambda=args.rate,num_classes=args.num_classes,img_size=data_config['input_size'])
+        else:
+            dataset_train = InversePoisonDatasetWrapper(dataset_train, patch_lambda=args.rate,num_classes=args.num_classes,img_size=data_config['input_size'])
 
     # create data loaders w/ augmentation pipeiine
     train_interpolation = args.train_interpolation
@@ -701,7 +708,6 @@ def main():
         use_multi_epochs_loader=args.use_multi_epochs_loader,
         worker_seeding=args.worker_seeding,
         patch_labmda=args.rate,
-        num_classes=args.num_classes
     )
     if utils.is_primary(args):
         subject.notify(settings.CREATE_LOADER_COMPLETE, data={
@@ -713,6 +719,26 @@ def main():
     if args.distributed and ('tfds' in args.dataset or 'wds' in args.dataset):
         # FIXME reduces validation padding issues when using TFDS, WDS w/ workers and distributed training
         eval_workers = min(2, args.workers)
+
+    if args.rate > 0:
+        dataset_eval_trigger, dataset_eval_clean = torch.utils.data.random_split(dataset_eval, round(args.test_rate * len(dataset_eval)))
+        dataset_eval = InversePoisonDatasetWrapper(dataset_eval_trigger, args.num_classes, 1.0, isTrain=False)
+        loader_eval_clean = create_loader(
+            dataset_eval_clean,
+            input_size=data_config['input_size'],
+            batch_size=args.validation_batch_size or args.batch_size,
+            is_training=False,
+            use_prefetcher=args.prefetcher,
+            interpolation=data_config['interpolation'],
+            mean=data_config['mean'],
+            std=data_config['std'],
+            num_workers=eval_workers,
+            distributed=args.distributed,
+            crop_pct=data_config['crop_pct'],
+            pin_memory=args.pin_mem,
+            device=device,
+        )
+    
     loader_eval = create_loader(
         dataset_eval,
         input_size=data_config['input_size'],
@@ -783,6 +809,8 @@ def main():
             decreasing=decreasing,
             max_history=args.checkpoint_hist
         )
+        if args.rate > 0:
+            saver.bd_metric = 0.0
         with open(os.path.join(output_dir, 'args.yaml'), 'w') as f:
             f.write(args_text)
 
@@ -853,8 +881,20 @@ def main():
                 args,
                 amp_autocast=amp_autocast,
                 epoch=epoch,
-                subject=subject
+                subject=subject,
+                is_poisoned=args.rate > 0
             )
+            if args.rate > 0:
+                eval_metrics_clean = validate(
+                    model,
+                    loader_eval_clean,
+                    validate_loss_fn,
+                    args,
+                    amp_autocast=amp_autocast,
+                    epoch=epoch,
+                    subject=subject,
+                    is_poisoned=False
+                )
 
             if model_ema is not None and not args.model_ema_force_cpu:
                 if args.distributed and args.dist_bn in ('broadcast', 'reduce'):
@@ -892,6 +932,12 @@ def main():
 
             if saver is not None:
                 # save proper checkpoint with eval metric
+                if args.rate > 0:
+                    save_metric = eval_metrics[eval_metric]
+                    clean_metric = eval_metrics_clean[eval_metric]
+                    if save_metric - clean_metric > saver.bd_clean:
+                        saver.bd_metric = save_metric - clean_metric
+
                 save_metric = eval_metrics[eval_metric]
                 best_metric, best_epoch = saver.save_checkpoint(epoch, metric=save_metric)
 
@@ -904,7 +950,8 @@ def main():
     if utils.is_primary(args):
         subject.notify(settings.PROGRAM_EXIT, data={
             settings.PROGRAM_EXIT_PARAM_HPARAM : args.__dict__,
-            settings.PROGRAM_EXIT_PARAM_BEST_METRIC : best_metric
+            settings.PROGRAM_EXIT_PARAM_BEST_METRIC : best_metric,
+            settings.PROGRAM_EXIT_PARAM_BD_METRIC : saver.bd_metric if hasattr(saver, 'bd_metric') else 0
         })
 
     if best_metric is not None:
@@ -1161,7 +1208,8 @@ def validate(
                            settings.VALIDATE_ONE_EPOCH_COMPLETE_PARAM_EPOCH: epoch,
                            settings.VALIDATE_ONE_EPOCH_COMPLETE_PARAM_LOSS: losses_m.avg,
                            settings.VALIDATE_ONE_EPOCH_COMPLETE_PARAM_TOP1 : top1_m.avg,
-                           settings.VALIDATE_ONE_EPOCH_COMPLETE_PARAM_TOP5 : top5_m.avg
+                           settings.VALIDATE_ONE_EPOCH_COMPLETE_PARAM_TOP5 : top5_m.avg,
+                           settings.VALIDATE_ONE_EPOCH_COMPLETE_PARAM_CLEAN : not is_poisoned,
                        }
                        )
     metrics = OrderedDict([('loss', losses_m.avg), ('top1', top1_m.avg), ('top5', top5_m.avg)])